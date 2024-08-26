4204a9144c6cdb96db75c139d1cc21388ba33956
Kazuma Yagi <kazulab10969@outlook.jp>
2024-08-26 18:41:35 +0900

    fix

--- src/whill_modelc_publisher.cpp
+++ src/whill_modelc_publisher.cpp
@@ -400,22 +400,24 @@ int main(int argc, char **argv)
 					whill_modelc_battery->publish(batteryState);
 
 					// Publish Odometry
-     if (publish_odom_tf)
-					{
-					 geometry_msgs::msg::TransformStamped odom_trans;
-					 odom_trans = odom.getROSTransformStamped();
-					 odom_trans.header.stamp.sec = RCL_NS_TO_S(now);
-					 odom_trans.header.stamp.nanosec = now - RCL_S_TO_NS(odom_trans.header.stamp.sec);
-					 odom_trans.header.frame_id = "odom";
-					 odom_trans.child_frame_id = "base_link";
-					 odom_broadcaster_.sendTransform(odom_trans);
-     }
-
 					odom_msg = odom.getROSOdometry();
 					odom_msg.header.stamp.sec = RCL_NS_TO_S(now);
 					odom_msg.header.stamp.nanosec = now - RCL_S_TO_NS(odom_msg.header.stamp.sec);
-					odom_msg.header.frame_id = "odom";
-					odom_msg.child_frame_id = "base_link";
+
+     				if (publish_odom_tf)
+					{
+						odom_msg.header.frame_id = "odom";
+						odom_msg.child_frame_id = "base_link";
+
+						geometry_msgs::msg::TransformStamped odom_trans;
+						odom_trans = odom.getROSTransformStamped();
+						odom_trans.header.stamp.sec = RCL_NS_TO_S(now);
+						odom_trans.header.stamp.nanosec = now - RCL_S_TO_NS(odom_trans.header.stamp.sec);
+						odom_trans.header.frame_id = "odom";
+						odom_trans.child_frame_id = "base_link";
+						odom_broadcaster_.sendTransform(odom_trans);
+     				}
+
 					whill_modelc_odom->publish(odom_msg);
 				}
 			}
