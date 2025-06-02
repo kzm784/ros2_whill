692146cb38dd7eb2c406e7d892c4ec13f706e043
Kazuma Yagi <kazulab10969@outlook.jp>
2025-06-02 14:29:01 +0900

    edit whill_launch.py

--- whill_bringup/launch/whill_launch.py
+++ whill_bringup/launch/whill_launch.py
@@ -12,29 +12,31 @@ import os
 
 def generate_launch_description():
 
-    model_file = os.path.join(get_package_share_directory('whill_description'), 'urdf', 'whill_model_cr2.urdf')
-    
+    model_file = os.path.join(
+        get_package_share_directory('whill_description'),
+        'urdf',
+        'whill_model_cr2.urdf'
+    )
+
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{'robot_description': open(model_file, 'r').read()}],
+            parameters=[{
+                'robot_description': open(model_file, 'r').read()
+            }],
             remappings=[('/joint_states', '/whill/joint_states')]
         ),
 
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            output='screen',
-            remappings=[('joint_states', '/whill/states/jointState')]
-        ),
-        
-        Node(
             package='whill_driver',
-            namespace='',
             executable='whill',
             name='whill',
-            parameters=[PathJoinSubstitution([FindPackageShare('whill_bringup'), 'config', 'params.yaml'])],
+            parameters=[PathJoinSubstitution([
+                FindPackageShare('whill_bringup'),
+                'config',
+                'params.yaml'
+            ])],
         ),
     ])
