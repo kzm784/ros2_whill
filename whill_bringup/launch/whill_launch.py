4da1c484b401b678f48730577facd66f13609ac9
Kazuma Yagi <kazulab10969@outlook.jp>
2025-06-02 14:04:02 +0900

    edit whill_launch.py

--- whill_bringup/launch/whill_launch.py
+++ whill_bringup/launch/whill_launch.py
@@ -6,10 +6,30 @@ from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+
+    model_file = os.path.join(get_package_share_directory('whill_description'), 'urdf', 'whill_model_cr2.urdf')
+    
     return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': open(model_file, 'r').read()}],
+            remappings=[('/joint_states', '/whill/joint_states')]
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            output='screen',
+            remappings=[('joint_states', '/whill/states/jointState')]
+        ),
+        
         Node(
             package='whill_driver',
             namespace='',
